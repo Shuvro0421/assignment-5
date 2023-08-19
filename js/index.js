1eca5a783d2f7635b4a50a8d7151b605dbea9ddf
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-08-20 01:12:35 +0600

    7th commit

--- js/index.js
+++ js/index.js
@@ -1,42 +1,36 @@
-document.getElementById('card-1').addEventListener('click' , function(){
-
-    priceElement('price-1');
-    elementName('Spoons');
-
-})
-
-document.getElementById('card-2').addEventListener('click' , function(){
-
-    priceElement('price-2');
-    elementName('Plates')
-
-})
-
-document.getElementById('card-3').addEventListener('click' , function(){
-
-    priceElement('price-3');
-    elementName('Home Cooker')
-})
-
-function priceElement(prices){
+function priceElement(prices, name) {
 
     const price = document.getElementById(prices);
     const priceValueString = price.innerText;
     const priceValue = parseFloat(priceValueString);
     const totalPrice = document.getElementById('total-price');
-    totalPrice.innerText = priceValue;
-
-}
-
-function elementName(name){
+    const totalPriceString = totalPrice.innerText;
+    const totalPriceNumber = parseFloat(totalPriceString);
+    const NewTotalPrice = priceValue + totalPriceNumber;
+    totalPrice.innerText = NewTotalPrice.toFixed(2);
     const valueName = document.getElementById('element-name');
     const count = valueName.childElementCount + 1;
     const p = document.createElement('p');
     p.innerHTML = `${count}. ${name}`;
     valueName.appendChild(p);
-    
 
 }
+
+// codes
+document.getElementById('card-1').addEventListener('click', function () {
+    priceElement('price-1', 'Spoons');
+})
+
+document.getElementById('card-2').addEventListener('click', function () {
+    priceElement('price-2', 'Plates');
+})
+
+document.getElementById('card-3').addEventListener('click', function () {
+
+    priceElement('price-3', 'Home Cooker');
+})
+
+
 
 
 
